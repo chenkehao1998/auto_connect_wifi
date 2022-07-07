cecaa4ba335610fa1fee1e5680d022f206e94c80
kehao <754772661@qq.com>
2022-07-07 20:58:14 +0800

    适配英文cmd模式 对wifi连接顺序根据信号强度进行优化

--- src/main/java/com/kehao/auto_connect_wifi/task/AutoConnectWifiTask.java
+++ src/main/java/com/kehao/auto_connect_wifi/task/AutoConnectWifiTask.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,27 +30,38 @@ public class AutoConnectWifiTask {
             //网络连接正常 不操作
             return;
         }
+        log.info("当前网络异常,执行连接wifi命令！");
         List<WifiHotspotEntity> wifiHotspotEntityList = wifiTool.getWifiNearBy().stream()
                 .filter(wifiHotspotEntity -> Strings.isNotBlank(wifiHotspotEntity.getName()))
-                .filter(wifiHotspotEntity -> wifiHotspotEntity.getSignal() >= 50).collect(Collectors.toList());
+                .filter(wifiHotspotEntity -> wifiHotspotEntity.getSignal() >= 50)
+                .sorted(Comparator.comparingInt(WifiHotspotEntity::getSignal))
+                .collect(Collectors.toList());
         List<String> profiles = wifiTool.getProfiles();
-        for (WifiHotspotEntity wifiHotspotEntity : wifiHotspotEntityList) {
+        log.info("获取到{}个WiFi信号，过滤没有记录过密码的WiFi",wifiHotspotEntityList.size());
+        List<WifiHotspotEntity> wifiHotspotCanConnectEntityList = wifiHotspotEntityList.stream()
+                .filter(wifiHotspotEntity -> profiles.contains(wifiHotspotEntity.getName())).collect(Collectors.toList());
+        if(wifiHotspotCanConnectEntityList.isEmpty()){
+            log.info("没有可以连接的WiFi");
+            return;
+        }
+        log.info("共有{}个wifi可以被连接,开始执行连接",wifiHotspotCanConnectEntityList.size());
+
+        for (WifiHotspotEntity wifiHotspotEntity : wifiHotspotCanConnectEntityList) {
             String wifiName = wifiHotspotEntity.getName();
-            log.info("尝试连接wifi：{}",wifiName);
-            if (profiles.contains(wifiName)) {
-                if (wifiTool.connectToHotspot(wifiName)) {
-                    //网络连接成功
-                    if(wifiTool.networkConnected()){
-                        log.info("wifi连接成功：{}",wifiName);
-                        return;
-                    }else {
-                        log.info("wifi连接成功：{},但是没有网络",wifiName);
-                    }
+            log.info("开始连接wifi：{}",wifiName);
+            if (wifiTool.connectToHotspot(wifiName)) {
+                //网络连接成功
+                if(wifiTool.networkConnected()){
+                    log.info("wifi连接成功：{}",wifiName);
+                    return;
                 }else {
-                    log.info("连接{}失败",wifiName);
+                    log.info("wifi连接成功：{},但是没有网络",wifiName);
                 }
+            }else {
+                log.info("连接{}失败",wifiName);
             }
         }
+
         log.info("已尝试所有wifi,但均未成功联网");
     }
 }
--- src/main/java/com/kehao/auto_connect_wifi/tool/WifiTool.java
+++ src/main/java/com/kehao/auto_connect_wifi/tool/WifiTool.java
@@ -2,16 +2,14 @@ package com.kehao.auto_connect_wifi.tool;
 
 import cn.hutool.core.util.RuntimeUtil;
 import com.kehao.auto_connect_wifi.pojo.WifiHotspotEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class WifiTool {
     /**
@@ -25,15 +23,17 @@ public class WifiTool {
         List<WifiHotspotEntity> wifiHotspotEntityList = new ArrayList<>();
         if(cmdResultArray.length>=1){
             for (int i = 1; i < cmdResultArray.length; i++) {
-                Map<String, String> wifiMap =
-                      Arrays.stream(cmdResultArray[i].trim().split("\r\n"))
-                            .filter(str -> str.contains(":"))
-                            .map(str -> str.split(":"))
+
+                List<String[]> originDataList = Arrays.stream(cmdResultArray[i].trim().split("\r\n"))
+                        .filter(str -> str.contains(":"))
+                        .map(str -> str.split(":")).collect(Collectors.toList());
+
+                Map<String, String> wifiMap = originDataList.stream().filter(arr->!arr[0].startsWith("         "))
                             .collect(Collectors.toMap(arr -> arr[0].trim(), arr -> arr[1].trim(),(v1,v2)->v1));
 
                 String wifiName = wifiMap.get("SSID "+i);
-                String signalStrengthStr = wifiMap.get("信号");
-                int signalStrength = Integer.parseInt(signalStrengthStr.replace("%",""));
+                int signalStrength = originDataList.stream().filter(arr->arr[0].contains("Signal")||arr[0].contains("信号"))
+                        .map(arr->Integer.parseInt(arr[1].trim().replace("%",""))).max((v1,v2)->v1-v2).get();
 
                 wifiHotspotEntityList.add(WifiHotspotEntity.builder()
                         .name(wifiName)
@@ -67,7 +67,7 @@ public class WifiTool {
     public boolean networkConnected() {
         String cmd = "ping www.baidu.com";
         String cmdResult = RuntimeUtil.execForStr(cmd);
-        List<String> list = Arrays.stream(cmdResult.split("\n")).filter(str -> str.contains("回复")).collect(Collectors.toList());
+        List<String> list = Arrays.stream(cmdResult.split("\n")).filter(str -> str.contains("回复")||str.contains("Reply")).collect(Collectors.toList());
         return list.size()>0;
     }
 
@@ -75,10 +75,16 @@ public class WifiTool {
      * 连接到WiFi热点
      * @param wifiName wifi名
      */
-    public boolean connectToHotspot(String wifiName){
+    public boolean connectToHotspot(String wifiName)  {
         String cmd = String.format("netsh wlan connect name=\"%s\"", wifiName);
         String connectResult = RuntimeUtil.execForStr(cmd);
-        if(connectResult.contains("已成功完成连接请求")){
+        if(connectResult.contains("已成功完成连接请求")||connectResult.contains("Connection request was completed successfully.")){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                log.info("连接WiFi：{} 发生错误",wifiName);
+            }
             return true;
         }
         return false;
